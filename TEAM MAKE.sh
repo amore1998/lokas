32ce9e912a21be404ec5cd0248da897228c227b8
hussian1997 <hhby413@gmail.com>
2017-01-14 18:02:41 +0300

    Add files via upload

---
+++ TEAM MAKE.sh
@@ -0,0 +1,9 @@
+عليك شهر رمضان لا تمسح اي حقوق 
+    
+    echo "@liberbot_iraq"
+    echo "@livil"
+    echo "تمت مسح ملف الملف ال state"
+    kill -9 $(pgrep telegram-cli)
+    rm -rf ~/.telegram-cli/state
+ 
+الملف بس ضيفه وين ما جان وشغله وخلاص   

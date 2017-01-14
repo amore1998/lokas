7c96ad482792b314a34a3f2dfde7e5c4d5d2d8c1
hussian1997 <hhby413@gmail.com>
2017-01-14 17:26:50 +0300

    Update config.lua

--- data/config.lua
+++ data/config.lua
@@ -1,5 +1,5 @@
 do local _ = {
-  about_text = "Welcome to DevPoint For more information Subscribe to the channel @DevPointTeam",
+  about_text = "Welcome to DevPoint For more information Subscribe to the channel @MEKE_CANALS",
   enabled_plugins = {
    "4",
     "APICLI",
